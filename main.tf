b148ea02b149e1ef2211e5580dc5bca960886ebc
Dexarc <76424945+Dexarc@users.noreply.github.com>
2021-05-27 18:16:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,4 +14,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > hosts.inv"
+  }
 }
